cbbf23d5576bf4c736c4747ef523afcfa2c082e0
KarmD <krd37@njit.edu>
2021-03-10 16:28:23 -0500

    Edit to the Random List with new random function

--- src/RandomGenerator/RandomList.js
+++ src/RandomGenerator/RandomList.js
@@ -1,7 +1,12 @@
-function RandomList(rand){
-
-    var rand = rand[Math.floor(Math.random() * rand.length)];
-
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
 }
-module.exports = rand;
 
+console.log(getRandomInt(3));
+// expected output: 0, 1 or 2
+
+console.log(getRandomInt(1));
+// expected output: 0
+
+console.log(Math.random());
+// expected output: a number from 0 to <1
