9260f44ffee6086e251fe5e29b2dd0e4e9fc1444
cjl72 <55719709+cjl72@users.noreply.github.com>
2021-03-10 12:59:37 -0500

    Task: Fixed randomListInt.js and randomListDec.js functions and test

--- src/RandomGenerator/RandomList.js
+++ src/RandomGenerator/RandomList.js
@@ -1,12 +1,11 @@
 const seedRandom = require('seedrandom');
-
 function randomListDec(a,b,length,sd){
     if(Number.isInteger(a)){
         return randomListInt(a, b, length, sd);
     }
     let lst = [];
+    seedRandom(sd, {global: true});
     for (i = 0; i < length; i++) {
-        seedRandom(sd);
         let num = (Math.random() * (b - a) + b);
         lst.push(num);
     }
@@ -14,8 +13,8 @@ function randomListDec(a,b,length,sd){
 }
 function randomListInt(a,b,length,sd){
     let lst = [];
+    seedRandom(sd, {global: true});
     for (i = 0; i < length; i++) {
-        seedRandom(sd);
         a = Math.ceil(a);
         b = Math.floor(b);
         let num = (Math.floor(Math.random() * (b - a) + a));
