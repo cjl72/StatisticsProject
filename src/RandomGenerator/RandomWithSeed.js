de47f8b79d4de3ceb23a981e6cc0a6c6b44d24d6
KarmD <krd37@njit.edu>
2021-03-10 22:25:45 -0500

    Task: RandomWithSeed.js update task

--- src/RandomGenerator/RandomWithSeed.js
+++ src/RandomGenerator/RandomWithSeed.js
@@ -1,5 +1,12 @@
-function RandomWithSeed{
-
+const seedRandom = require
+function RandomWithSeed(data, num, sd) {
+    seedrandom(sd);
+    let lst = [];
+    for (i = 0; i < num; i++){
+        let x = Math.floor(Math.random() * Math.floor(data))
+    }
 }
+
+
 
 module.exports = RandomWithSeed;
