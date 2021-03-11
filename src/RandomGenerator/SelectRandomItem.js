e102a849b6997947535c1917c36a301eb99b78ff
cjl72 <55719709+cjl72@users.noreply.github.com>
2021-03-10 21:09:33 -0500

    Task: Fixed

--- src/RandomGenerator/SelectRandomItem.js
+++ src/RandomGenerator/SelectRandomItem.js
@@ -1,4 +1,8 @@
 function SelectRandomItem(data) {
+    if (data.length === 0){
+        throw 'Error: The array is empty';
+    }
+
     let l = data.length;
     let num = Math.floor(Math.random() * Math.floor(l-1))
     return data[num];
