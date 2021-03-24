4d26d698a5bac5bfc70bb815b980c393955fcd4e
karmdesai <krd37@njit.edu>
2021-03-23 21:20:05 -0400

    Added SimpleSampling.js

---
+++ src/PopSamplingFunctions/SimpleSampling.js
@@ -0,0 +1,10 @@
+import "../RandomGenerator/SelectWithoutSeed"
+
+const seedRandom = require('seedrandom');
+
+function SimpleSampling(seedRandom){
+    return (
+        seed(sd)
+    )
+}
+export default SimpleSampling;
